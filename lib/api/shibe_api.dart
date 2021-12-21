7a06066b13d4760b405520016a2d177684efd8c5
q <mboh.dong@gmail.com>
2021-12-21 13:30:30 +0700

    increase api call item

--- lib/api/shibe_api.dart
+++ lib/api/shibe_api.dart
@@ -6,7 +6,7 @@ class ShibeApi {
   Future getShibe() async {
     try {
       final res = await dio.get(
-          'https://shibe.online/api/shibes?count=10&urls=true&httpsUrls=true');
+          'https://shibe.online/api/shibes?count=25&urls=true&httpsUrls=true');
 
       return res.data;
     } on Exception catch (_) {
